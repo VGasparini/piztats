13cff084f0d5297f9761b8c51ac4446e24481132
GUSTAVO MIQUELLUZZI BONASSA <09845549985@edu.udesc.br>
2019-10-03 12:19:44 -0300

    Ta funcionando adicionar pizza

---
+++ src/Cards/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+`;
+export const Tabela = styled.table`
+    max-width: 500px;
+    width: 100%;
+    margin-top: 10px;
+`;
